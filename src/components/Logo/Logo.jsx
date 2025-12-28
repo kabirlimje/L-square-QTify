c0230372811ff879882d7c3b5019f08c128162a0
kabir limje <kabirlimje133@gamil.com>
2025-12-28 13:16:24 +0530

    Navbar and Hero section

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Logo.module.css";
+import LogoImage from "../../assets/Group 3748.png";
+
+export default function Logo() {
+  return <img src={LogoImage} alt="logo" width={67} />;
+}
